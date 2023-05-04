103df565087a94df3013af1d572a6b7d8d1995ee
islammd39e <islammdrahat308@gmail.com>
2023-05-04 09:39:31 +0600

    04/05/23

--- assets/js/main.js
+++ assets/js/main.js
@@ -120,3 +120,53 @@ listItems.forEach(item =>{
 // };
 
 // export default SideBarItems;
+
+// import { createBrowserRouter } from "react-router-dom";
+// import HRM from "../pages/HRM";
+// import Home from "../pages/Home";
+// import RootComponent from "../rootComponent/RootComponent";
+// import Inbox from "../pages/Inbox";
+// import List1 from "../pages/List1";
+// import List2 from "../pages/List2";
+// import List3 from "../pages/List3";
+// import Inbox1 from "../pages/Inbox1";
+// import Inbox2 from "../pages/Inbox2";
+// import Inbox3 from "../pages/Inbox3";
+// import MenuOptionList from "./_nav";
+
+// const getFlattenRoutes = (MenuOptionList: any) =>
+//   MenuOptionList.filter((item: any) => item.isRoute)
+//     .map((route: any) => [
+//       route.children ? getFlattenRoutes(route.children) : [],
+//       route,
+//     ])
+//     .flat(Infinity);
+
+// export const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: <RootComponent />,
+//     children:
+//       MenuOptionList &&
+//       getFlattenRoutes(MenuOptionList).map((item: any) => ({
+//         path: item.path,
+//         element: item.element,
+//       })),
+
+//     // children: [
+//     //   { path: "/dashboard", element: <Home /> },
+//     //   { path: "/parties", element: <HRM /> },
+//     //   { path: "/monitoring", element: <Inbox /> },
+//     //   { path: "/users", element: <HRM /> },
+//     //   { path: "/settings", element: <List1 /> },
+//     //   { path: "/parties/customers", element: <List2 /> },
+//     //   { path: "/parties/suppliers", element: <List3 /> },
+//     //   { path: "/inbox/inbox1", element: <Inbox1 /> },
+//     //   { path: "/inbox/inbox2", element: <Inbox2 /> },
+//     //   { path: "/inbox/inbox3", element: <Inbox3 /> },
+//     // ],
+//   },
+//   { path: "/login", element: <h3>Login</h3> },
+//   { path: "/register", element: <h3>Register</h3> },
+// ]);
+
